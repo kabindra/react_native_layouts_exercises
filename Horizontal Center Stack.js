b8c891388127ffb9994c1fbaab2e4dadc763e2cf
Kabindra Shrestha <kabindra.stha@gmail.com>
2019-08-07 15:23:17 -0500

    Update Horizontal Center Stack.js
    Color code correction

--- Horizontal Center Stack.js
+++ Horizontal Center Stack.js
@@ -5,7 +5,7 @@ export default class HorizontalCenterStack extends Component {
   render() {
     return (
       <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-        <View style={{width: 116, height: 116, backgroundColor: '#116E3C2'}} />
+        <View style={{width: 116, height: 116, backgroundColor: '#16E3C2'}} />
         <View style={{width: 116, height: 116, backgroundColor: '#4A90E2'}} />
         <View style={{width: 116, height: 116, backgroundColor: '#9013FE'}} />
       </View>
